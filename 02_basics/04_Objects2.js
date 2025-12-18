13bc1ec31697d44f0222a7def4ac59da5ec534b0
Rahul Patil <sudhakarpatil1247@gmail.com>
2025-12-18 14:24:45 +0000

    Objects level 2

---
+++ 02_basics/04_Objects2.js
@@ -0,0 +1,61 @@
+// const tinderUser = new Object()  // singleton object
+
+const tinderUser = {} // non- singleton object
+
+
+tinderUser.id = "123tfdsx"
+tinderUser.name = "Rahul"
+tinderUser.email = "rahulspatil@345"
+tinderUser.isloggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUsers = {
+    email: "Banduemail.com",
+    fullname : {
+        userFullname : {
+            firstname : "Rahul",
+            lastname : "Patil",
+        }
+    }
+}
+// console.log(regularUsers.fullname.userFullname.firstname);
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+const obj4 = {5 : "c", 6 : "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1 , obj2, obj4) // it's help to marging 
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+
+const Users = [
+    {
+        id : 20,
+        email : "Rahul@google.com"
+
+    },
+    {
+        id : 20,
+        email : "Rahul@google.com"
+
+    },
+    {
+        id : 20,
+        email : "Rahul@google.com"
+
+    }
+]
+ 
+Users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('rollNo'));
+
