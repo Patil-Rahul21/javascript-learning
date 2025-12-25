384412ccb706cfd6f177e9ff41da1034a58a64e8
Rahul Patil <sudhakarpatil1247@gmail.com>
2025-12-25 05:38:03 +0000

    IIFE

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,12 @@
+//Immediately Invoke Function Expressions (IIFE)
+
+(function chai() {
+    // named IIFE
+    console.log(`DB Connected`);
+    
+})(); //
+
+
+((name) => {                               // if you run this funtion then end the above code first using semicolon ;
+    console.log(`DB Connected tWo ${name}`);
+})('Rahul')
