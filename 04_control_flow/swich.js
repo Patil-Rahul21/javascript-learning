a43d5965b0e153094dba29521e28cb300a39806f
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-01-03 01:48:53 +0000

    Swich-Cases

---
+++ 04_control_flow/swich.js
@@ -0,0 +1,34 @@
+// const month =  2;
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     default:
+//         console.log("Default Value");
+//         break;
+// }
+const month =  "Feb";
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+
+    default:
+        console.log("Default Value");
+        break;
+}
