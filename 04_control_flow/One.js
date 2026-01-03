3992472eacfc658a9ce50dd6ade1e32518800eda
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-01-03 01:48:33 +0000

    Conditions

---
+++ 04_control_flow/One.js
@@ -0,0 +1,62 @@
+// if
+
+// const isUserlogin = true
+
+// const temperature = 40
+
+// if (temperature === 42) {
+//     console.log("less than 50"); 
+// } else {
+//     console.log("temprature is greater than 50");
+    
+// }
+
+// console.log("Executedd");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if (score > 100) {
+
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+      
+// }
+// console.log(`User Power: ${power}`);
+
+const balance = 1000
+
+// implisit scope
+// if(balence > 700) console.log("Yes"), console.log("No");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1111");
+// }
+
+const isUserlogin = true;
+const debitCard = true;
+const loggedInFromGoogle = false
+const loggedInFromEmail = true;
+
+// if(isUserlogin && debitCard && 2 == 3) // && it means all condition will be true
+//     {   
+//     console.log("Allow to buy course");
+// }
+
+if(loggedInFromGoogle || loggedInFromEmail){ 
+    // || it means any one condition will true
+    console.log("user logged in");
+    
+}
+
+
+
