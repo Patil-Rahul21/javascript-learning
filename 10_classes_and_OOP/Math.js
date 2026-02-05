ffe9df7bd103f6edcd3fb05865060f9959f8ea59
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-02-05 07:30:14 +0000

    Math and decripter

---
+++ 10_classes_and_OOP/Math.js
@@ -0,0 +1,32 @@
+const decripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(decripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+
+const chai = {
+    name: "chaa",
+    peice: 20,
+    isAvailable: true,
+    orderChai : function(){
+        console.log("chai nahi bani hai");
+        
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    writable : false,
+    enumerable : false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+ if (typeof value !== 'function'){
+    console.log(`${key} : ${value}`);
+    }
+}
