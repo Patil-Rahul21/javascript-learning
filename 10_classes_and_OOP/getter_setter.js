16a7ab1b8374dbf4e08fcd52a767f8b1d826183c
Rahul Patil <sudhakarpatil1247@gmail.com>
2026-02-05 07:31:11 +0000

    Getter and Setter

---
+++ 10_classes_and_OOP/getter_setter.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}Rahul`
+    }
+
+    set password(value){
+        this._password = value  
+    }
+ 
+}
+
+const rahul = new User("rahul@gmail.com", "12345")
+console.log(rahul.password);
+console.log(rahul.email);
